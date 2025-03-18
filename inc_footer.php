20f7eba33990ee17baf71247e4149ff19d3afa3f
mr-Choi07 <chltmdwo2007@naver.com>
2025-03-18 14:38:53 +0900

    Update inc_footer.php

--- inc_footer.php
+++ inc_footer.php
@@ -2,9 +2,9 @@
         <footer class="py-3 my-4">
           <ul class="nav justify-content-center border-top pb-3 mb-3">
             <p class="text-muted nav-link mt-2">&copy; 2019 Company, Inc</p>
-            <p class="text-muted nav-link mt-2">부산광역시 수영구 광안로41번길 7-1 1층</p>
-            <p class="text-muted nav-link mt-2">대표: 최승재</p>
-            <p class="text-muted nav-link mt-2">Tel: 010-2164-5040</p>
+            <p class="text-muted nav-link mt-2">부산광역시</p>
+            <p class="text-muted nav-link mt-2">대표: innocent</p>
+            <p class="text-muted nav-link mt-2">Tel: 010-0000-0000</p>
             <p class="text-muted nav-link mt-2">e-mail: chltmdwo2007@naver.com</p>
           </ul>
         </footer>
